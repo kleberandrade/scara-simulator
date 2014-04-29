381d28d0b3c9262486ef7077bbfa9afd96ad4e30
Kleber Andrade <pdjkleber@gmail.com>
2014-04-29 16:19:49 -0300

    Foram unidos os scripts de MoveAxis1, 2 e 3 no script RobotAxis.

--- Assets/Scripts/RobotAxis.cs
+++ Assets/Scripts/RobotAxis.cs
@@ -4,15 +4,48 @@ using System.Collections;
 public class RobotAxis : MonoBehaviour
 {
 	public AxisType axisType = AxisType.None;
+	public string axisName = string.Empty;
+	[Range(-360.0f, 360.0f)]
+	public float origin = 0.0f;
+	[Range(0.0f, 360.0f)]
+	public float range = 0.0f;
 
-	void Start () 
-	{
-	
-	}
+	public float height = 0.0f;
+
+	private float position = 0.0f;
+	private float movement = 0.0f;
+
 
 	void Update () 
 	{
-	
+		movement += Input.GetAxis (axisName);
+
+		switch (axisType) 
+		{
+			case AxisType.Rotate:
+				position = RotateAxis(origin, movement);
+				transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, position, transform.localRotation.eulerAngles.z);
+				break;
+
+			case AxisType.Translate:
+				position = TranslateAxis(movement);
+				transform.localPosition = new Vector3(transform.localPosition.x, position, transform.localPosition.z);
+				break;
+		}
+	}
+
+	private float RotateAxis(float origin, float angle)
+	{
+		float result = origin - angle;
+		if (result < 360.0f)
+			return 360.0f + result;
+		return result;
+	}
+
+	float TranslateAxis(float position)
+	{
+		float result = origin + ((origin - height) * (-position) / range);
+		return Mathf.Clamp(result, height, origin);
 	}
 }
 
